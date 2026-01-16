68907cab8149a95fe143b2fafeaf79f566183b35
LendroDuCode <deto.karamoko@sonecafrica.com>
2026-01-16 11:59:12 +0000

    First commit

---
+++ app/(back-office)/dashboard-agent/villes/[uuid]/page.tsx
@@ -0,0 +1,8 @@
+"use client"; // si c’est un composant client
+import React from "react";
+
+const VillePage = () => {
+  return <div>Page VillePage {/* ton contenu */}</div>;
+};
+
+export default VillePage; // ✅ Obligatoire
