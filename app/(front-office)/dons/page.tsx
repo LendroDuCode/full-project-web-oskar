1c87dd8cc416921818e5d1aae174a556b9e5de49
LendroDuCode <deto.karamoko@sonecafrica.com>
2026-01-16 12:10:54 +0000

    First commit

---
+++ app/(front-office)/dons/page.tsx
@@ -0,0 +1,8 @@
+"use client"; // si c’est un composant client
+import React from "react";
+
+const DonPage = () => {
+  return <div>Page DonPage {/* ton contenu */}</div>;
+};
+
+export default DonPage; // ✅ Obligatoire
